d7bbd28e6c88bac877de648caa7431db0d7d6f80
Taylor Hunt <62562348+Huntaylor@users.noreply.github.com>
2026-03-06 13:49:45 -0700

    adds ui package
    adds scripts and others

---
+++ get_packages.sh
@@ -0,0 +1,20 @@
+dart pub global activate derry
+dart pub add flame
+dart pub add equatable_annotations
+dart pub add bloc
+dart pub add copy_with_extension
+dart pub add equatable
+dart pub add flame_behaviors
+dart pub add flame_bloc
+dart pub add flame_splash_screen
+dart pub add flame_tiled
+dart pub add flex_color_scheme
+dart pub add flutter_bloc
+dart pub add go_router
+dart pub add go_router_paths
+dart pub add logging
+dart pub add --dev equatable_gen
+dart pub add --dev build_runner
+dart pub add --dev copy_with_extension_gen
+dart pub add --dev flame_lint
+dart pub add --dev very_good_analysis
